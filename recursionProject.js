ee4a59fadadf116dd67312ceaf59ca0940b6abb4
Didier <115608922+DogGotSwag@users.noreply.github.com>
2024-10-07 21:22:05 -0600

    complete fibsRec & returns correct answer
    also fixed the if that checks if num is 0, changed
    to less or equal 0 in case user give negative num

--- recursionProject.js
+++ recursionProject.js
@@ -1,20 +1,20 @@
-// function iterationFibs(num) {
-//   if (num === 0) return [];
-//   let arr = [];
-//   let x = 0;
-//   let y = 1;
-//   for (let i = 0; i < num; i++) {
-//     arr.push(x);
-//     [x, y] = [y, x + y];
-//   }
-//   return arr;
-// }
+function iterationFibs(num) {
+  if (num <= 0) return [];
+  let arr = [];
+  let x = 0;
+  let y = 1;
+  for (let i = 0; i < num; i++) {
+    arr.push(x);
+    [x, y] = [y, x + y];
+  }
+  return arr;
+}
 
-// console.log(iterationFibs(8));
+console.log(iterationFibs(8));
 
-function fibsRec(num){
-
+function fibsRec(num, x = 0, y = 1) {
+    if(num <= 0) return [];
+    return [x].concat(fibsRec(num-1,y,x+y));
 }
 
 console.log(fibsRec(8));
-
