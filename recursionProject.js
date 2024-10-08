5404a81494a06dd886ddbdc2fdd55bdb0c747fbe
Didier <115608922+DogGotSwag@users.noreply.github.com>
2024-10-07 21:12:30 -0600

    create fibsRec for the recursive version
    create the boiler plate that will be used. Does
    the same as iterationFibs but using recursion

--- recursionProject.js
+++ recursionProject.js
@@ -1,13 +1,20 @@
-function iterationFibs(num) {
-  if (num === 0) return [];
-  let arr = [];
-  let x = 0;
-  let y = 1;
-  for (let i = 0; i < num; i++) {
-    arr.push(x);
-    [x, y] = [y, x + y];
-  }
-  return arr;
+// function iterationFibs(num) {
+//   if (num === 0) return [];
+//   let arr = [];
+//   let x = 0;
+//   let y = 1;
+//   for (let i = 0; i < num; i++) {
+//     arr.push(x);
+//     [x, y] = [y, x + y];
+//   }
+//   return arr;
+// }
+
+// console.log(iterationFibs(8));
+
+function fibsRec(num){
+
 }
 
-console.log(iterationFibs(8));
+console.log(fibsRec(8));
+
