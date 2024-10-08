df600639a2c8cdbde2c156929e75e731fed9d520
Didier <115608922+DogGotSwag@users.noreply.github.com>
2024-10-07 21:09:42 -0600

    finish code for iterationFibs
    does the first 8 of the sequence correctly

--- recursionProject.js
+++ recursionProject.js
@@ -1,5 +1,13 @@
-function iterationFibs(num){
-
+function iterationFibs(num) {
+  if (num === 0) return [];
+  let arr = [];
+  let x = 0;
+  let y = 1;
+  for (let i = 0; i < num; i++) {
+    arr.push(x);
+    [x, y] = [y, x + y];
+  }
+  return arr;
 }
 
-console.log(iterationFibs());
+console.log(iterationFibs(8));
